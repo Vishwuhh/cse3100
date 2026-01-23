5582055cfddd98a2eeaa24254050989079904266
Vishwuhh <vishwabalamurugan12@gmail.com>
2026-01-23 11:00:14 -0500

    Lab 0

---
+++ Lab 0/lab0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num = 2;
+    int sum = 0;
+
+    while (num < 200) {
+        sum += num;
+        num += 2;
+    }
+
+    printf("Hello World!\n");
+    printf("The sum is: %i\n", sum);
+    return 0;
+}
