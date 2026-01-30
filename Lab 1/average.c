0f77d593d403220bf8bfa7d0b586e7e87bfd35d6
Vishwuhh <vishwabalamurugan12@gmail.com>
2026-01-30 10:27:49 -0500

    Coded average.c based on requirements

--- Lab 1/average.c
+++ Lab 1/average.c
@@ -1,1 +1,17 @@
 // Only this line of comment is provided 
+#include <stdio.h>
+
+int main(void) {
+    double x; // declares all inputs as double
+    double total = 0.0; // sum of all inputs
+    double average = 0.0; // solution when dividing total by number of inputs
+    int count = 0; // number of inputs
+
+    while (scanf("%lf", &x == 1)) { // checks if the input is a double
+        count += 1;
+        total += x;
+        average += total/count;
+    }
+
+    printf("Total=%f Average=%f\n", total, average); // prints the running total and average
+}
