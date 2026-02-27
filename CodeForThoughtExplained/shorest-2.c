713e741451795bf9ed453eaa00dea8268ffb83b5
Vishwuhh <155852834+Vishwuhh@users.noreply.github.com>
2026-02-27 03:15:01 +0000

    Explained shortest-2 with comments

--- CodeForThoughtExplained/shorest-2.c
+++ CodeForThoughtExplained/shorest-2.c
@@ -40,25 +40,34 @@ t_pair *shortest_dist_pair(int n, t_point points[])
 	int i, j;
 	double d;
 	t_pair *p_pair;
-	p_pair = malloc(sizeof(t_pair));
+	p_pair = malloc(sizeof(t_pair)); // allocates the memory on the heap for the pairs
 	if(p_pair == NULL)
 	{
 		perror("Cannot allocate memory.\n");
 		exit(-1);
 	}
 
+	// assuming the first two points in the array are closest to the start
+	// the & gets the address of where the points are on the array for min_d calculation
 	double min_d = squared_dist(&(points[0]), &(points[1]));
+	// goes to the p_pair heap memory address, finds the one assoicated with p1/p2 and puts the value of points associated with ind 1 or 0
 	p_pair -> p1 = points[0];
 	p_pair -> p2 = points[1];
-	// TODO fill in the code
-
-
-
-
-
-
-
-
+	
+	for (i = 0; i < n; i++) { // efficiency of O(n^2) for the unique pairs loop
+		// starts at i + 1 to avoid self-comparions and duplicates
+		for (j = i + 1; j < n; j++) {
+			// calculate distance for the current paur using addresses found using &
+	        double current_d = squared_dist(&(points[i]), &(points[j])); 
+	        if (current_d < min_d) { // updates the minimum
+	            min_d = current_d; // saves new distance
+				// copies the struct data int the heap-allocated pair
+				// goes to the p_pair memory heap, finds associated place with p1/p2 and puts value of i and j in the points array
+	            p_pair->p1 = points[i];
+	            p_pair->p2 = points[j];
+	        }
+	    }
+	}
     return p_pair;
 }
 //Do not change the following code
@@ -79,9 +88,10 @@ int main(int argc, char *argv[])
 	srand(12345);
 	for(i=0; i<n; i++)
 	{
-		points[i].id = i;
-		points[i].x = ((double)rand())/RAND_MAX;
-		points[i].y = ((double)rand())/RAND_MAX;
+		// no arrows needed since points[i] is NOT a pointer, but a structure
+		points[i].id = i; // goes to the struct and finds the id value and sets it to i
+		points[i].x = ((double)rand())/RAND_MAX; // go to the struct and find the slot inside points[i] with x-coor
+		points[i].y = ((double)rand())/RAND_MAX; // same as line above for the y-coor
 	}
 
 	t_pair *p_pair;
