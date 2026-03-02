04d316832ed42a42c5fb0f157fe43f774cf001d1
Vishwuhh <155852834+Vishwuhh@users.noreply.github.com>
2026-03-02 11:41:31 -0500

    explaining code logic
    Updated comments for clarity and fixed minor typos.

--- Exam 1 - WORK/place.c
+++ Exam 1 - WORK/place.c
@@ -9,23 +9,26 @@ void print_elements(char **elems, int count) {
 
 // from notes/HuskyCT
 int integerCompare(const void *a, const void *b) {
+  // array of pointers, so need to cast * to get the address of the target and get the string stored there
   char *s1 = *(char **)a;
   char *s2 = *(char **)b;
 
   int val1, val2;
 
-  // sscanf from notes
+  // sscanf from notes, helps parse strings into integrs
   sscanf(s1, "%d", &val1); // like scanf, but takes tge already given inputs instead of waiting for user
   sscanf(s2, "%d", &val2);
 
   // if reuturning negative, val1 is smaller
   // if returning positive, val2 is bigger
+  // was for the first case to get half credit
   return val2 - val1; 
 }
 
 int main(int argc, char *argv[]) {
+  // stopping segmentation faults
   if (argc < 2) {
-    perror("Usage : ./place <strings>");
+    perror("Usage : ./place <strings>"); // added from notes, rushed
     exit(1);
   }
 
